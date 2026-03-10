18cabf9e033b9e015b201cd95003881a62615218
MernNasa <mernnasa@gmail.com>
2026-03-11 02:19:22 +0530

    backend url changed to vercel

--- src/API/axiosInstace.js
+++ src/API/axiosInstace.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL: "https://projectlc-0ft2.onrender.com",
+    baseURL: "https://project-lc.vercel.app",
     withCredentials: true,
 });
